5cb7cc1e7914cbbdeb67d2c3ad0726b2900891a5
Dexter <48792150+DexterWatson@users.noreply.github.com>
2023-09-12 17:32:02 +0100

    Update api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -266,6 +266,7 @@ export const historyEnsure = async (): Promise<CosmosDBHealth> => {
     .then(async res => {
         let respJson = await res.json();
         let formattedResponse;
+        console.log(respJson);
         if(respJson.message){
             formattedResponse = CosmosDBStatus.Working
         }else{
